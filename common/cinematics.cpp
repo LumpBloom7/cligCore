a082a321c5124fe9e5889519de98bb5fc8a3aad0
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-04-02 16:25:48 +0800

    Fixed undefined reference

--- common/cinematics.cpp
+++ common/cinematics.cpp
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream:core.cpp
-#include "core.hpp"
-
-#include <fstream>
-#include <sys/stat.h>
-
-#include "termcolor.hpp"
-
-// Includes platform specific implementations
-#if defined( _WIN32 ) || defined( _WIN64 )
-#include "winCore.cpp"
-#elif defined( __unix__ ) || defined( __unix )
-#include "linuxCore.cpp"
-#endif
-
-namespace cligCore {
-  namespace filesystem {                           // Functions used to read and modify the file system of the user.
-    bool fileExists( const std::string &fileName ) // Used to check if a certain file exists with a certain directory
-    {
-      struct stat buffer;
-      return ( stat( fileName.c_str(), &buffer ) == 0 );
-    }
-  } // namespace filesystem
-} // namespace cligCore
-=======
 namespace cligCore {
   namespace cinematics {
     class Event {
@@ -43,4 +18,3 @@ namespace cligCore {
     }
   } // namespace cinematics
 } // namespace cligCore
->>>>>>> Stashed changes:common/cinematics.cpp
