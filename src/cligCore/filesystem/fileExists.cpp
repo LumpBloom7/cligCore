f568b54b94e2b00e57b15b1cf48c090a568c9ab2
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2019-10-11 19:24:32 +0800

    Wiped everything and use MSBuild instead

--- src/cligCore/filesystem/fileExists.cpp
+++
@@ -1,7 +0,0 @@
-#include "clig.hpp"
-bool cligCore::filesystem::fileExists(
-    const std::string &fileName ) // Used to check if a certain file exists with a certain directory
-{
-  struct stat buffer;
-  return ( stat( fileName.c_str(), &buffer ) == 0 );
-}
