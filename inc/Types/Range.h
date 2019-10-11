27216b1cbdceee63104346e6fb3705e92ce350b3
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2019-10-11 20:07:10 +0800

    Add Range type

--- inc/Types/Range.h
+++ inc/Types/Range.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <string>
+#include "input.h"
+#include "console.h"
+namespace cligCore::types {
+	class Range {
+	public:
+		Range(int lowerBounds, int upperBounds, bool isSelectable = false);
+		Range(int lowerBounds, int upperBounds, int currentVal);
+		Range();
+		int getLower();
+		int getUpper();
+		void setLower(int value);
+		void setUpper(int value);
+		void shift(int value);
+		void showChooser(const std::string& title);
+		int getCurrent();
+
+	private:
+		int _current;
+		int _lower, _upper;
+		bool _isSelectable = false;
+	};
+}
---
+++ src/types/Range.cpp
@@ -0,0 +1,66 @@
+#include "Types/Range.h"
+
+namespace cligCore::types {
+	Range::Range() : _lower(0), _upper(10), _current(0), _isSelectable(false) {}
+	Range::Range(int lowerBounds, int upperBounds, bool isSelectable) {
+		// Differentiate the smaller value from the bigger value to ease processing and to avoid any potential bugs.
+		_lower = (lowerBounds <= upperBounds) ? lowerBounds : upperBounds;
+		_upper = (lowerBounds <= upperBounds) ? upperBounds : lowerBounds;
+		_isSelectable = isSelectable;
+		_current = _lower;
+	}
+	Range::Range(int lowerBounds, int upperBounds, int currentVal) {
+		// Differentiate the smaller value from the bigger value to ease processing and to avoid any potential bugs.
+		_lower = (lowerBounds <= upperBounds) ? lowerBounds : upperBounds;
+		_upper = (lowerBounds <= upperBounds) ? upperBounds : lowerBounds;
+		_isSelectable = true;
+		_current = (currentVal >= _lower && currentVal <= _upper) ? currentVal : _lower;
+	}
+	int Range::getLower() { return _lower; }
+	int Range::getUpper() { return _upper; }
+
+	void Range::setLower(int value) { _lower = value; }
+	void Range::setUpper(int value) { _upper = value; }
+
+	void Range::shift(int value) {
+		_lower += value;
+		_upper += value;
+	}
+
+	void cligCore::types::Range::showChooser(const std::string& title) {
+		cligCore::console::clear();
+		int current = _current;
+		std::cout << title << "\n"
+			<< "Please select a value between " << _lower << " and " << _upper << "." << std::endl
+			<< std::endl
+			<< " < " << current << " > \r" << std::flush;
+
+		bool failsafe = false;
+		while (true) {
+			if (!failsafe) {
+				cligCore::input::getKeyInput();
+				failsafe = true;
+			}
+			switch (cligCore::input::getKeyInput()) {
+			case cligCore::input::Keys::Right: {
+				if (current < _upper) current++;
+
+				std::cout << " < " << current << " > \r" << std::flush;
+				break;
+			}
+			case cligCore::input::Keys::Left: {
+				if (current > _lower) current--;
+
+				std::cout << " < " << current << " > \r" << std::flush;
+				break;
+			}
+			case cligCore::input::Keys::Enter: {
+				_current = current;
+			}
+			case cligCore::input::Keys::Escape: {
+				return;
+			}
+			}
+		}
+	}
+}
